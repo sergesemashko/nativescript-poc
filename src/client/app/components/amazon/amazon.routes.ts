961dd8e59d2287426bb016bbe7830e5ddd1411ec
sergesemashko <semashkosergey@gmail.com>
2017-10-12 12:39:56 -0400

    Initial amazon route commit

---
+++ src/client/app/components/amazon/amazon.routes.ts
@@ -0,0 +1,8 @@
+import { AmazonComponent } from './amazon.component';
+
+export const AmazonRoutes: Array<any> = [
+  {
+    path: 'amazon',
+    component: AmazonComponent
+  }
+];
--- src/client/app/components/app.routes.ts
+++ src/client/app/components/app.routes.ts
@@ -1,8 +1,10 @@
 // app
 import { HomeRoutes } from './home/home.routes';
 import { AboutRoutes } from './about/about.routes';
+import { AmazonRoutes } from './amazon/amazon.routes';
 
 export const routes: Array<any> = [
   ...HomeRoutes,
-  ...AboutRoutes
+  ...AboutRoutes,
+  ...AmazonRoutes
 ];
